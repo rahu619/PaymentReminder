63a5640c3df9ca84b73f4be4be653a431f5c422d
Rahul <rahu619@gmail.com>
2018-09-23 20:32:29 +1200

    initial

---
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/ContentViewActivity.java
@@ -0,0 +1,13 @@
+package com.google.android.gms.samples.vision.ocrreader;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ContentViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_view);
+    }
+}
---
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/Details.java
@@ -0,0 +1,49 @@
+package com.google.android.gms.samples.vision.ocrreader;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Details extends Activity implements DatePickerDialog.OnDateSetListener {
+    DatePickerDialog datePickerDialog;
+    EditText _date;
+    ImageView ivHome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        datePickerDialog =new DatePickerDialog(this,Details.this,2018,1,1);
+
+        _date=findViewById(R.id.billdate);
+
+        ivHome=findViewById(R.id.ivhome);
+        ivHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent _home=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(_home);
+                finish();
+
+            }
+        });
+    }
+
+
+
+    public void dateSelect(View v){
+            datePickerDialog.show();
+    }
+
+
+    @Override //set selected date
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+      String _dateformat=String.format("%d-%d-%d", dayOfMonth,month,year);
+      _date.setText(_dateformat);
+    }
+}
